e6a1034564e01bb2f12bf2151eb58f7e7776f015
firefoxy <firefoxy31@yandex.ru>
2023-04-13 18:35:06 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -8,22 +8,5 @@
 
 // Коммит второй
 
-typedef struct vector {
-    int *data;
-    size_t size;
-    size_t capacity;
-} Vector;
-
-Vector createVector(size_t n);
-
-void reserve(Vector *v, size_t newCapacity);
-
-void clear(Vector *v);
-
-void shrinkToFit(Vector *v);
-
-void deleteVector(Vector *v);
-
-
 
 #endif //UNTITLED_VECTOR_H
