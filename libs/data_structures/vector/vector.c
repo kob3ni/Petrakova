743a04640e843f8ea35c8c73456c211c8c2a5292
firefoxy <firefoxy31@yandex.ru>
2023-04-13 18:37:16 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -3,3 +3,40 @@
 
 // Коммит второй
 
+Vector createVector(size_t n) {
+    int *data = (int*) malloc(sizeof(int) * n);
+    if (data == NULL) {
+        fprintf(stderr, "Wrong value: capacity");
+        exit(1);
+    }
+    return (Vector) {
+            data,
+            0,
+            n
+    };
+}
+
+void reserve(Vector *v, size_t newCapacity) {
+    if (newCapacity == 0) v->data = NULL;
+    v->data = (int*) realloc(v->data ,sizeof(int) * newCapacity);
+    if (v->data == NULL) {
+        fprintf(stderr, "Wrong value: newCapacity = %llu", newCapacity);
+        exit(1);
+    }
+    v->capacity = newCapacity;
+    if (newCapacity < v->size) v->size = newCapacity;
+}
+
+void clear(Vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(Vector *v) {
+    v->capacity = v->size;
+}
+
+void deleteVector(Vector *v) {
+    free(v->data);
+    v->size = 0;
+    v->capacity = 0;
+}
