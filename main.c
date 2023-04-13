202a78f01ee36d566c6fe98ba5e5cb0096854d5b
firefoxy <firefoxy31@yandex.ru>
2023-04-13 18:02:52 +0300

    refactoring

--- main.c
+++ main.c
@@ -140,7 +140,7 @@ void test() {
     test_pushBack_emptyVector();
     test_pushBack_fullVector();
     test_popBack_notEmptyVector();
-    test_atVector_nonEmptyVector();
+    test_atVector_notEmptyVector();
     test_atVector_requestToLastElement();
     test_back_oneElementInVector();
     test_front_oneElementInVector();
